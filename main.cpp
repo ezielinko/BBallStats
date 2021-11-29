97da14d2ac664156cd5ade258abe53dba9cd70ac
Eryk Zielinko <eryk.zielinko@gmail.com>
2021-11-29 23:42:41 +0100

    Simplifed data in LA stats.txt Add getline in loop to add data from file into porgram operations

--- main.cpp
+++ main.cpp
@@ -1,18 +1,28 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-#include "declarations.hpp"
+#include "functions.cpp"
 
 using namespace std;
 
 int main() {
-    fstream myFile("LAL_vs_Pistons");
-
+    fstream myFile("LA stats.txt");
     if(myFile.is_open()){
         cout << "File opened!" << endl;
     } else {
         cout << "Error, could not open the file!" << endl;
         exit(1);
+    }
+
+    string input;
+    while (getline(myFile, input)){
+        for(int i = 0; i < input.size(); i++){
+            if(input[i] == '%'){
+                input[i] = '0';
+                input[i] = input[i - 2] / input[i - 1] * 100;
+            }
+        }
+        cout << input << endl;
     }
 
     myFile.close();

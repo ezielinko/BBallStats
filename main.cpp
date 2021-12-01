49b50b48b1a9dc1386e2b9b31fef9337ec012508
Eryk Zielinko <eryk.zielinko@gmail.com>
2021-12-01 23:36:58 +0100

    Add working loop to fill vector from file to calculate accuracy points Add loop to print result

--- main.cpp
+++ main.cpp
@@ -1,35 +1,49 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <iomanip>
 #include "functions.cpp"
 
 using namespace std;
 
 int main() {
     fstream myFile("LA stats.txt");
-    fstream playerStats ("Player list.txt");
+    fstream playerStats("Player list.txt");
 
     cout << myFile.is_open() << endl;
     cout << playerStats.is_open() << endl;
 
-    vector<int> stats;
-    int input;
-    int counter = 0;
+    vector<double> stats;
+    double input;
+    int lineCounter = 0;
+    int sizeCounter = 0;
 
 
 
-    while (myFile >> input){
-        if(counter == 8) {
-            double avg2pts = stats[6] * stats[7] / 100;
-            stats.push_back((double)avg2pts);
-        } else if (counter == 11){
-            double avg3pts = stats[9] * stats[10] / 100;
-            stats.push_back((double)avg3pts);
+    while (myFile >> input) {
+        if (lineCounter == 8) {
+            double avg2pts = stats[sizeCounter - 1] / stats[sizeCounter - 2];
+            stats.push_back(avg2pts);
+        } else if (lineCounter == 11) {
+            double avg3pts = stats[sizeCounter - 1] / stats[sizeCounter - 2];
+            stats.push_back(avg3pts);
         } else {
             stats.push_back(input);
         }
-        counter++;
+        if (lineCounter == 15){
+            lineCounter = 0;
+        }
+        lineCounter++;
+        sizeCounter++;
 
+
+    }
+
+    for (int i = 0; i < stats.size(); i++) {
+        if(i % 3 == 0 && i % 5 == 0) {
+            cout << "\n";
+        }
+        cout << fixed << stats[i] << " ";
     }
 
     myFile.close();

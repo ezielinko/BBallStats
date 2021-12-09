75db96e74b97ec13acc0543d2954fc212bc1606d
Eryk Zielinko <eryk.zielinko@gmail.com>
2021-12-09 16:33:16 +0100

    add some descriptions in template.txt file. Add declarations for calculate PER functions for individual positions.

--- declarations.hpp
+++ declarations.hpp
@@ -3,6 +3,10 @@ using namespace std;
 
 #ifndef BASKETBALLSTATS_DECLARATIONS_HPP
 #define BASKETBALLSTATS_DECLARATIONS_HPP
-
+void pgPerPattern(vector<double> stats);
+void shPerPattern(vector<double> stats);
+void sfPerPattern(vector<double> stats);
+void pfPerPattern(vector<double> stats);
+void cPerPattern(vector<double> stats);
 
 #endif
--- functions.cpp
+++ functions.cpp
@@ -1,2 +1,8 @@
 #include "declarations.hpp"
 // Jak zrobić funkcję przyjmująca plik/nazwe pliku
+
+void pgPerPattern(vector<double> stats);
+void shPerPattern(vector<double> stats);
+void sfPerPattern(vector<double> stats);
+void pfPerPattern(vector<double> stats);
+void cPerPattern(vector<double> stats);
